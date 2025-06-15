d9ee440ebe6db5135b2a51f4b7a8e4238b0787db
LIONS19 <shuntaro.t1022@gmail.com>
2025-06-16 00:43:56 +0900

    add ui/home-template

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignIn from "./components/forms/LoginForm";
 import SignUp from "./components/forms/SignUpForm";
+import Dashboard from "./components/templates/dashboard/Dashboard";
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/home" element={<Dashboard />} />
     </Routes>
   </Router>
 );
