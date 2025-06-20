fdb3d1329704dff55e86b5f167a607697d7ba1c1
LIONS19 <shuntaro.t1022@gmail.com>
2025-06-20 22:35:29 +0900

    get home

--- my-app/src/utils/api.tsx
+++ my-app/src/utils/api.tsx
@@ -2,3 +2,21 @@ export const fetchUsers = async () => {
   const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/users`);
   return await res.text();
 };
+
+export const fetchAllPosts = async () => {
+  const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/all-posts`, {
+    method: 'GET',
+    credentials: 'include',
+  });
+  if (!res.ok) throw new Error('投稿取得失敗');
+  return await res.json();
+};
+
+export const fetchMyPosts = async () => {
+  const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/my-posts`, {
+    method: 'GET',
+    credentials: 'include',
+  });
+  if (!res.ok) throw new Error('自分の投稿取得失敗');
+  return await res.json();
+};
